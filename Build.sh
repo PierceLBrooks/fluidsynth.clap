e2be45c8100f2993d649fb56ec259ec72c6ca9c8
Pierce Brooks <piercebrooks@rocketmail.com>
2024-11-09 13:56:00 -0500

    build everything in accordance with variants/deployments/flavors/policies

--- Build.sh
+++ Build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python3 -m pip install -r $PWD/Buildster/requirements.txt
-python3 $PWD/Buildster/buildster/buildster.py $PWD/Project.xml
+python3 $PWD/Buildster/buildster/buildster.py $PWD/Project.xml BUILDSTER_VARIANT=$1
 
